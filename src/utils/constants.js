7bcddd93bed6a3dca4513a33a54d330211a7696a
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-05-24 11:01:03 +0800

    added constants, libs, and apps

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const INTRO_SCREEN_01 = {
+  title: '',
+  description: '',
+};
+
+export const INTRO_SCREEN_02 = {
+  title: '',
+  description: '',
+};
+
+export const LOGIN_SCREEN = {
+  title: '',
+  description: '',
+};

2011a2f860fceedb103e169c19eee40e2dfafb08
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-05-25 12:46:19 +0800

    remove unused imports

--- src/screens/LastIntroScreen.jsx
+++ src/screens/LastIntroScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { LAST_INTRO_SCREEN } from '../utils/constants';
